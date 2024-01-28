179b57897a2d94dd1ed98489c94f8e58fecf0316
namyoungjun96 <ehdgornltls@naver.com>
2024-01-28 13:12:20 +0900

    백준 1,2,3더하기 품

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,77 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] cache;
 
-    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
-        int n = 10;
-        cache = new int[n+1];
-        cache[2] = 1;
-        cache[3] = 1;
-        Arrays.fill(cache, Integer.MAX_VALUE);
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
+//
+//        int testcase = Integer.parseInt(br.readLine());
+//        int[] answer = new int[testcase];
+//        for(int t=0; t<testcase; t++) {
+//            int n = Integer.parseInt(br.readLine());
+//            int[] cache = new int[11];
+//            cache[1] = 1;
+//            cache[2] = 2;
+//            if(n < 3)
+//                answer[t] = cache[n];
+//            else
+//                answer[t] = solution(n, cache);
+//        }
+//
+//        for(int i : answer)
+//            System.out.println(i);
+        int n = 7;
+        int[] cache = new int[n+1];
+        cache[1] = 1;
+        cache[2] = 2;
 
-        System.out.println(solution(n));
+        System.out.println(solution(n, cache));
     }
 
-    public static int solution(int x) {
-        if(x == 1) return 0;
-        if(cache[x] != Integer.MAX_VALUE) return cache[x];
+    public static int solution(int x, int[] cache) {
+        if(x == 0) return 1;
+        if(cache[x] != 0) return cache[x];
 
-        if(x%3 == 0) cache[x] = Math.min(solution(x/3) + 1, cache[x]);
-        if(x%2 == 0) cache[x] = Math.min(solution(x/2) + 1, cache[x]);
-
-        cache[x] = Math.min(solution(x-1) + 1, cache[x]);
+        cache[x] = solution(x-1, cache) + solution(x-2, cache) + solution(x-3, cache);
 
         return cache[x];
     }
 }
-
-// 12 /3 /2 4 6
+// n = solution[x-1] + solution[x-2] + 1;
+// 1 2 4 7 13?
+// - 1
+// - 2
+// 1 1
+// 2
+// - 3
+// 1 1 1
+// 1 2
+// 2 1
+// 3
+// - 4
+// 1+1+1+1
+// 1+1+2
+// 1+2+1
+// 2+1+1
+// 2+2
+// 1+3
+// 3+1
+/* 5
+* 1 1 1 1 1
+* 1 1 1 2
+* 1 1 2 1
+* 1 2 1 1
+* 2 1 1 1
+* 1 2 2
+* 2 1 2
+* 2 2 1
+* 1 3 1
+* 3 1 1
+* 1 1 3
+* 2 3
+* 3 2
+*/
