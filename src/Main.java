f2df418a3ff1fc32eeae9577c11ab94c18d9bd39
namyoungjun96 <ehdgornltls@naver.com>
2024-02-11 12:29:07 +0900

    백준 주유소 성공

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st;
-//
-//        int testcase = Integer.parseInt(br.readLine());
-//        int[] answer = new int[testcase];
-//        for(int t=0; t<testcase; t++) {
-//            int n = Integer.parseInt(br.readLine());
-//            int[] cache = new int[11];
-//            cache[1] = 1;
-//            cache[2] = 2;
-//            if(n < 3)
-//                answer[t] = cache[n];
-//            else
-//                answer[t] = solution(n, cache);
-//        }
-//
-//        for(int i : answer)
-//            System.out.println(i);
-        int n = 7;
-        int[] cache = new int[n+1];
-        cache[1] = 1;
-        cache[2] = 2;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        System.out.println(solution(n, cache));
+        int testcase = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        long[] distance = new long[testcase-1];
+        for(int i=0; i<testcase-1; i++) {
+            distance[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+        long[] cities = new long[testcase];
+        for(int i=0; i<testcase; i++) {
+            cities[i] = Integer.parseInt(st.nextToken());
+        }
+
+//        int n = 4;
+//        int[] distance = {2, 3, 1};
+//        int[] cities = {5, 2, 4, 1};
+
+        System.out.println(solution(distance, cities));
     }
 
-    public static int solution(int x, int[] cache) {
-        if(x == 0) return 1;
-        if(cache[x] != 0) return cache[x];
+    public static long solution(long[] distance, long[] cities) {
+        long answer = 0;
+        long cost = cities[0];
 
-        cache[x] = solution(x-1, cache) + solution(x-2, cache) + solution(x-3, cache);
+        for (int i=0; i<distance.length; i++) {
+            if(cities[i] < cost)
+                cost = cities[i];
 
-        return cache[x];
+            answer += distance[i] * cost;
+        }
+
+        return answer;
     }
 }
-// n = solution[x-1] + solution[x-2] + 1;
-// 1 2 4 7 13?
-// - 1
-// - 2
-// 1 1
-// 2
-// - 3
-// 1 1 1
-// 1 2
-// 2 1
-// 3
-// - 4
-// 1+1+1+1
-// 1+1+2
-// 1+2+1
-// 2+1+1
-// 2+2
-// 1+3
-// 3+1
-/* 5
-* 1 1 1 1 1
-* 1 1 1 2
-* 1 1 2 1
-* 1 2 1 1
-* 2 1 1 1
-* 1 2 2
-* 2 1 2
-* 2 2 1
-* 1 3 1
-* 3 1 1
-* 1 1 3
-* 2 3
-* 3 2
-*/
