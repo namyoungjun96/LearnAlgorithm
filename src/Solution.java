3134f07dedcb3b491a9097baa6b75addd8c74446
namyj96 <fuswns96@mobigen.com>
2024-03-18 17:25:08 +0900

    프로그래머스 줄 서는 방법중

--- src/Solution.java
+++ src/Solution.java
@@ -1,66 +1,63 @@
 import java.util.Arrays;
-import java.util.PriorityQueue;
 
 class Solution {
     public static void main(String[] args) {
-        solution();
+        System.out.println(Arrays.toString(solution(3, 5)));
     }
 
-    public static void solution() {
-        int p = 20;
-        int fullPizza = (1 << p) - 1;
-        System.out.println(fullPizza);
-        System.out.printf(Integer.toBinaryString(fullPizza) + "\n");
+    static int count = 0;
+    static int[] answer;
 
-        int toppings = (1 << p) -1;
-        toppings |= (1 << p);
-        System.out.println(toppings);
-        System.out.printf(Integer.toBinaryString(toppings) + "\n");
+    public static int[] solution(int n, long k) {
+        int[] temp = new int[n];
+        int[] output = new int[n];
+        boolean[] visited = new boolean[n];
+        for (int i = 1; i <= n; i++) {
+            temp[i-1] = i;
+        }
 
-        if((toppings & (1 << p)) > 0)
-            System.out.println("pepperoni is in");
+        permutation(temp, 0, n, n);
+        return answer;
+    }
 
-        toppings -= (1 << p);
-        System.out.println(toppings);
-        System.out.printf(Integer.toBinaryString(toppings) + "\n");
+    static void permutation(int[] arr, int depth, int n, int r) {
+        if (depth == r) {
+            System.out.println(Arrays.toString(arr) + ", " + r);
+            return;
+        }
 
-        toppings &= ~(1 << p);
-        System.out.println(toppings);
-        System.out.printf(Integer.toBinaryString(toppings) + "\n");
+        int count = 0;
 
-        toppings ^= (1 << p);
-        System.out.println(toppings);
-        System.out.printf(Integer.toBinaryString(toppings) + "\n");
+        for (int i = depth; i < n; i++) {
+            swap(arr, depth, i);
+            permutation(arr, depth + 1, n, r);
+            swap(arr, depth, i);
+        }
+    }
 
-        int a = (1 << 4) -1;
-        int b = (1 << 3);
-        int added = (a | b) ;       // 합집합
-        int intersection = (a & b); // 교집합
-        int removed = (a &~ b);      // 차집합
-        int toolged = (a ^ b);      // a와 b중 하나에만 포함된 원소들의 집합
+    static void swap(int[] arr, int depth, int i) {
+        int temp = arr[depth];
+        arr[depth] = arr[i];
+        arr[i] = temp;
+    }
 
-        System.out.println("================================================");
-        System.out.printf(Integer.toBinaryString(a) + "\n");
-        System.out.printf(Integer.toBinaryString(b) + "\n");
-        System.out.println("합집합: "+ added);
-        System.out.println("교집합: "+ intersection);
-        System.out.println("차집합: "+ removed);
-        System.out.println("a와 b중 하나에만 포함된 원소들의 집합: "+ toolged);
-        System.out.println(Integer.bitCount(a));
+    static void perm(int[] arr, int[] output, boolean[] visited, int depth, int n, int r, long k) {
+        if (depth == r) {
+            count ++;
+            if(count == k) {
+                answer = output;
+            }
+            return;
+        }
+
+        for (int i=0; i<n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                output[depth] = arr[i];
+                perm(arr, output, visited, depth + 1, n, r, k);
+                output[depth] = 0;
+                visited[i] = false;
+            }
+        }
     }
 }
-// 1~9 X는 바다, 숫자는 무인도
-// 3 <= maps.length <= 100
-// 3 <= maps[i].length() <= 100
-// maps[i] == x || 1 ~ 9
-// 지도는 직사각형 == 가로, 세로가 다를수도 있다.
-// input ex == ["X591X","X1X5X","X231X", "1XXX1"]
-// input ex == ["XXX","XXX","XXX"]
-// 각 숫자는 머물 수 있는 날수다. 오름차순으로 머물 수 있는 날 수를 출력
-
-/*
-    1. 상하좌우 중 붙어 있으면 더하는 섬이다.
-    2. 탐색은 상하좌우만 하면 된다.
-    3. 캐싱을 해 탐색한 섬은 체크한다.
-    4. 최대가 아니라 머물 수 있는 전체를 계산해야 한다.
- */
